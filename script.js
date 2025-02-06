428e0dc93fc529d5c81704fa5b15c0fa603c074e
Beinngg <bing.com.edu.sg@gmail.com>
2025-02-06 20:46:33 +0800

    fixed calling function

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function login() {
+  const APIKEY = "67a447880b037f5d6b192c9d"; 
+  const APIURL = "https://desktop-87aa.restdb.io/rest/userinfo"; 
     //Get email info//
     let emailentered= document.getElementById("email").value
 
-    //GET code//
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://desktop-87aa.restdb.io/rest/userinfo",
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        }
-      }
-      
-      $.ajax(settings).done(function (data) {
+    let settings = {
+      method: "GET", //[cher] we will use GET to retrieve info
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": APIKEY,
+        "Cache-Control": "no-cache"
+      },
+    }
+
+    //[STEP 8]: Make our AJAX calls
+    // Once we get the response, we modify our table content by creating the content internally. We run a loop to continuously add on data
+    // RESTDb/NoSql always adds in a unique id for each data; we tap on it to have our data and place it into our links 
+    fetch(APIURL, settings)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+
+        window.location.href = "profile.html"
+
       });
-
-      }
-
-const APIKEY = "67a447880b037f5d6b192c9d"; 
-const APIURL = "https://desktop-87aa.restdb.io/rest/userinfo"; 
-
+    }
