828b65655b571edabf36ea5ef328e78e4613e5fa
zenonkwok <s10267477@connect.np.edu.sg>
2025-02-07 14:42:43 +0800

    Completed signup feature and linked to restdb API

--- script.js
+++ script.js
@@ -48,3 +48,62 @@ function login() {
       alert("An error occurred. Please try again later.");
     });
 }
+
+
+function signUp() {
+  const APIKEY = "67a447880b037f5d6b192c9d";
+  const form =  document.getElementById("register");
+
+  form.addEventListener("submit", function (e) {
+    // Check if the form is valid
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      e.preventDefault();
+      return;
+    }
+  })
+
+  e.preventDefault();
+  
+  let username = document.getElementById("username").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let password = document.getElementById("password").value.trim();
+
+  //Preparing the data before it's sent to the server
+  let jsondata = {
+    Username: username,
+    Email: email,
+    Password: password,
+  };
+  console.log(jsondata);
+
+  let settings = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": APIKEY,
+    },
+    body: JSON.stringify(jsondata),
+  };
+
+  // Send data to restdb api
+  fetch("https://desktop-87aa.restdb.io/rest/userinfo", settings)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      alert("User registered successfully!");
+      window.location.href = "signup.html";
+    })
+    .catch(error => {
+      console.error("Error registering user:", error);
+      alert("An error occurred. Please try again later.");
+    });
+
+
+
+
+
+
+
+
+}
